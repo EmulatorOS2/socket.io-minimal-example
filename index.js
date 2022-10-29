82114b524f494782aef6fa5960d27650a1d6b41d
KSalvador 06757 <102191619+EmulatorOS@users.noreply.github.com>
2022-10-29 17:15:15 +0000

    fix

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var express = require("express"); // use express
 var app = express(); // create instance of express
 var server = require("http").Server(app); // create server
 var io = require("socket.io")(server); // create instance of socketio
+const port = process.env.PORT || 4000
 var count=0;
+
 
 app.get( "/",function(req,res){
   res.sendFile(__dirname+"/index.html");
@@ -19,6 +21,6 @@ io.on("connection", socket => {
   });
 });
 
-server.listen(4000,function(){
-  console.log("listening on 3000");
-}) // run server
+server.listen(port, () => {
+  console.log(`EmulatorOS is running at localhost:${port}`)
+})
